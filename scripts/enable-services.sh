f3e1acbbf509998706256eaa604aa83b590f3251
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-08-02 12:03:02 -0300

    chore: update services to enable

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -3,9 +3,10 @@
 echo "Enabling services..."
 
 sudo systemctl enable bluetooth.service
+sudo systemctl enable cronie
 sudo systemctl enable cups
 sudo systemctl enable docker
-sudo systemctl enable sddm
 sudo systemctl enable power-profiles-daemon.service
+sudo systemctl enable sddm
 
 echo "Services enabled."
