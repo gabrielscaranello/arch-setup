b8b042d4682b720fdbe8ba9427c0bed0a8b2f590
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-08-02 17:27:50 -0300

    chore: remove swapfile from swap setup

--- scripts/setup-swap.sh
+++ scripts/setup-swap.sh
@@ -7,16 +7,8 @@ echo "Setting up swap..."
 
 echo "Removing old files"
 sudo swapoff --all
-sudo rm -rf /swapfile
-sudo sed -i '/^\/swapfile/d' /etc/fstab
 sudo sed -i '/^zram-size/d' /etc/systemd/zram-generator.conf
 sudo rm -rf /etc/sysctl.d/00-custom.conf
-
-echo "Creating swapfile"
-sudo fallocate -l ${SIZE}G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 echo "Updating zram-generator..."
 echo "zram-size=max(ram/2, $MB_SIZE)" | sudo tee -a /etc/systemd/zram-generator.conf
