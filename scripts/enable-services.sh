08a351375e752588fc35a10c2fee14652ab28fb8
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-08-02 21:05:40 -0300

    feat: add new services to startup

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -6,7 +6,14 @@ sudo systemctl enable bluetooth.service
 sudo systemctl enable cronie
 sudo systemctl enable cups
 sudo systemctl enable docker
+sudo systemctl enable fancontrol.service
 sudo systemctl enable power-profiles-daemon.service
 sudo systemctl enable sddm
+sudo systemctl enable systemd-boot-update.service
+sudo systemctl enable systemd-time-wait-sync.service
+sudo systemctl enable thermald.service
+sudo systemctl enable ufw.service
+
+sudo ufw enable
 
 echo "Services enabled."
