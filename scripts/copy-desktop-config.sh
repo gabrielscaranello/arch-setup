ab5013097c5c7b13869a6b9360b256d63e0fe918
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-09-05 02:53:12 -0300

    chore: update desktop config script

--- scripts/copy-desktop-config.sh
+++ scripts/copy-desktop-config.sh
@@ -5,5 +5,14 @@ echo "Copying desktop config..."
 
 echo "Copying..."
 cp "$PWD"/config/mimeapps.list ~/.config/mimeapps.list
+sudo cp "$PWD"/config/mimeapps.list /root/.config/mimeapps.list
+
+echo "Defing SYSTEMD_EDITOR"
+
+if grep -q "^SYSTEMD_EDITOR=" /etc/environment; then
+    sudo sed -i 's|^SYSTEMD_EDITOR=.*|SYSTEMD_EDITOR=nvim|' /etc/environment
+else
+    echo "SYSTEMD_EDITOR=nvim" | sudo tee -a /etc/environment > /dev/null
+fi
 
 echo "Desktop config setup done."
