25d6f713bac9ff5cd794226c49cd22e534e40ba2
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-09-05 21:40:48 -0300

    feat: update plasma config

--- scripts/copy-plasma-config.sh
+++ scripts/copy-plasma-config.sh
@@ -5,5 +5,10 @@ PWD=$(pwd)
 echo "Copying plasma config..."
 
 rsync -a "$PWD/plasma/config/" "$HOME/.config/"
+rsync -a "$PWD/plasma/local/" "$HOME/.local/"
+
+echo "Replace start icon"
+ICON_PATH="$HOME/.icons/start-here.svg"
+sed -i "s|:start-here-icon:|$ICON_PATH|g" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
 
 echo "Plasma config copied."
