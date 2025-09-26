66b585d6864c13092f5562fccfa6fb4f89bd9f28
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-09-26 18:09:18 -0300

    chore: update packages and scripts

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -7,7 +7,6 @@ sudo systemctl enable cronie
 sudo systemctl enable cups
 sudo systemctl enable docker
 sudo systemctl enable gdm
-sudo systemctl enable systemd-boot-update.service
 sudo systemctl enable systemd-time-wait-sync.service
 sudo systemctl enable ufw.service
 
